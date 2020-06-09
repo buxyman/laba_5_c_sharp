0f085d7d95102df761c7bf6ee6972a3456522b1c
buxyman <65020313+buxyman@users.noreply.github.com>
2020-06-09 15:40:47 +0300

    Add files via upload

---
+++ laba5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace programming_paradigms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Human [] human = new Human[10];
+            for(int i =0; i<10; i++) 
+                if (i % 5 == 0)
+                    human[i] = new Human("Max", "Chmurau");
+                else if (i % 5 == 1)
+                    human[i] = new StudentBSUIR("Pavel","Clybic");
+                else if (i % 5 == 2)
+                    human[i] = new AIStudent("Berejnov", "Danila");
+                else if (i % 5 == 3)
+                    human[i] = new IiTPStudent("Igar", "Karpenko");
+                else if (i % 5 == 4)
+                    human[i] = new ASOIStudent("I", "I");
+            foreach(Human i in human)
+                i.GetInfo();
+            foreach (Human i in human)
+                if (i is StudentBSUIR stud)
+                    Console.WriteLine(stud.Specialty);
+        }
+    }
+}
